57d2a63953076ee0eb8f01e5932a05b869d3aad5
luiiz2 <luizarturpombo13@gmail.com>
2025-04-06 13:58:34 -0300

    testado e funcioando, corrigido alguns erros.

--- Ouvidoria-Samu-Luiz.py
+++ Ouvidoria-Samu-Luiz.py
@@ -1,6 +1,6 @@
 from operacoesbd import *
  
-conexao = criarConexao("localhost", "rote", "19042005", "ouvidoria")
+conexao = criarConexao("localhost", "root", "19042005", "ouvidoria")
 opcao = 0
 while opcao != 7:
     print("escolha uma das opçoes abaixo:"
@@ -13,23 +13,23 @@ while opcao != 7:
     "\n7 - Sair do Sistema")
     opcao = int(input("Digite a opção desejada: "))
     if opcao == 1:
-        listarManifestações = "SELECT * FROM manifestacao"
+        listarManifestações = "SELECT * FROM manifestacoes"
         listagem = listarBancoDados(conexao, listarManifestações)
-        if len(listagem) == 0:
+        if len(listagem) <= 0:
             print("Não há manifestações cadastradas")
         else:       
             print("lista de manifestações")
             contagem = 0
             for manifestacao in listagem:
                 contagem += 1
-                print(contagem,"-", manifestacao[0])
+                print(f"{contagem} - Nome: {manifestacao[0]} | Tipo: {manifestacao[1]} | Código: {manifestacao[2]} | Manifestação: {manifestacao[3]}")
     elif opcao == 2:
         tipo = input("Digite o tipo de manifestação: reclamação, elogio ou sugestão: ")
         if tipo not in ["reclamação", "elogio", "sugestão"]:
             print("Tipo inválido")
             continue
         dados = [tipo]
-        listarManifestaçõesTipo = "SELECT * FROM manifestacao WHERE tipo = %s"
+        listarManifestaçõesTipo = "SELECT * FROM manifestacoes WHERE tipo = %s"
         listagem = listarBancoDados(conexao, listarManifestaçõesTipo, dados)
         if len(listagem) == 0:
             print("Não há manifestações cadastradas desse tipo")
@@ -38,7 +38,7 @@ while opcao != 7:
             contagem = 0
             for manifestacao in listagem:
                 contagem += 1
-                print(contagem,"-", manifestacao[0] )
+                print(f"{contagem} - Nome: {manifestacao[0]} | Tipo: {manifestacao[1]} | Código: {manifestacao[2]} | Manifestação: {manifestacao[3]}")
     elif opcao == 3:
         nome = input("Digite o nome do usuário: ")
         tipo = input("Digite o tipo de manifestação: reclamação, elogio ou sugestão: ")
@@ -47,28 +47,36 @@ while opcao != 7:
             print("Tipo inválido")
             continue
         dados = [nome, tipo, manifestacao]
-        inserirManifestacao = "INSERT INTO manifestacao (nome, tipo, manifestacao) VALUES (%s, %s, %s)"
+        inserirManifestacao = "INSERT INTO manifestacoes (nome, tipo, manifestaçao) VALUES (%s, %s, %s)"
         insertNoBancoDados(conexao, inserirManifestacao, dados)
         print("Manifestação cadastrada com sucesso")
     elif opcao == 4:
-        listarContagem = "SELECT COUNT(*) FROM manifestacao"
+        listarContagem = "SELECT COUNT(*) FROM manifestacoes"
         contagem = listarBancoDados(conexao, listarContagem)
         print("Total de manifestações cadastradas:", contagem[0][0])
     elif opcao == 5:
         codigo = int(input("Digite o código da manifestação: "))
         dados = [codigo]
-        listarManifestacaoCodigo = "SELECT * FROM manifestacao WHERE codigo = %s"
+        listarManifestacaoCodigo = "SELECT * FROM manifestacoes WHERE codigo = %s"
         listagem = listarBancoDados(conexao, listarManifestacaoCodigo, dados)
         if len(listagem) == 0:
-            print("Não há manifestações cadastradas com esse código")
+            print("Não há manifestação cadastrada com esse código")
         else:       
             print("manifestação encontrada")
             for manifestacao in listagem:
-                print(manifestacao[0])
+               print(f"""
+    -------------------------------
+    Código:         {manifestacao[2]}
+    Nome:           {manifestacao[0]}
+    Tipo:           {manifestacao[1]}
+    Manifestação:   {manifestacao[3]}
+    -------------------------------
+    """)
+
     elif opcao == 6:
         codigo = int(input("Digite o código da manifestação que deseja excluir: "))
         dados = [codigo]
-        excluirManifestacao = "DELETE FROM manifestacao WHERE codigo = %s"
+        excluirManifestacao = "DELETE FROM manifestacoes WHERE codigo = %s"
         linhasAfetadas = excluirBancoDados(conexao, excluirManifestacao, dados)
         if linhasAfetadas > 0:
             print("Manifestação excluída com sucesso")
